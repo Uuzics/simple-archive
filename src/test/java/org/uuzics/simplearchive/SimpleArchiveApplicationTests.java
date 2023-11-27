aba9c161696b541779ad7693db44e87252f36158
Uuzics <87236102+Uuzics@users.noreply.github.com>
2023-11-27 11:53:19 +1100

    Spring Boot init

---
+++ src/test/java/org/uuzics/simplearchive/SimpleArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uuzics.simplearchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
